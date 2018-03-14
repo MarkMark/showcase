74b998d8f4bf3d6297c0951282395ab490e5a92f
Mark <hisshash@gmail.com>
2018-03-14 11:38:15 -0600

    Feat (main): building main

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Main extends Component {
+
+  render() {
+    return (
+
+      <main>
+        <p>This is main</p>
+      </main>
+
+    )
+  }
+
+}
+
+export default Main;
