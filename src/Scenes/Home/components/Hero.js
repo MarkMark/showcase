6548297cb1ee422f8c4c34c38348c78701cd71a2
Mark <hisshash@gmail.com>
2018-03-18 11:14:45 -0600

    Feat (hide): responsive hide

--- src/Scenes/Home/components/Hero.js
+++ src/Scenes/Home/components/Hero.js
@@ -14,7 +14,11 @@ class Hero extends Component {
         <div className="c-hero c-hero--home">
           <div className="o-wrapper">
 
-            <h1 className="u-giga">Front End Dev<span className="u-responsive-hidden--hide-mobile">elopment</span></h1>
+            <h1 className="u-giga">
+              Front End
+              <span className="u-responsive-hidden--show-mobile u-responsive-hidden--show-mobile--inline"> Dev</span>
+              <span className="u-responsive-hidden--hide-mobile u-responsive-hidden--hide-mobile--inline"> Development</span>
+            </h1>
             <p>I build startups and companies great <b>websites & apps</b>.</p>
 
             <NavLink className="c-button" to="/about">
