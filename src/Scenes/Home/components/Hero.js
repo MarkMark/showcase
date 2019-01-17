62923ae827cec3c6a967fdb4bd748b86fc67ef47
Mark <hisshash@gmail.com>
2019-01-16 23:22:23 -0500

    Refactor (Components): convert to Functional components

--- src/Scenes/Home/components/Hero.js
+++ src/Scenes/Home/components/Hero.js
@@ -1,41 +1,33 @@
-import React, { Component } from 'react'
-import { NavLink }          from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-import Linebreak            from '../../../Components/Linebreak/Linebreak'
+import Linebreak from '../../../Components/Linebreak/Linebreak'
 
 
-class Hero extends Component {
+function Hero() {
+  return (
+    <div>
+      <div className="c-hero c-hero--home">
+        <div className="o-wrapper">
 
-  render() {
+          <h1 className="u-giga">
+            Front End
+            <span className="u-responsive-hidden--show-mobile u-responsive-hidden--show-mobile--inline"> Dev</span>
+            <span className="u-responsive-hidden--hide-mobile u-responsive-hidden--hide-mobile--inline"> Development</span>
+          </h1>
+          <p>I build startups and companies great <b>websites & apps</b>.</p>
 
-    return (
-
-      <div>
-        <div className="c-hero c-hero--home">
-          <div className="o-wrapper">
-
-            <h1 className="u-giga">
-              Front End
-              <span className="u-responsive-hidden--show-mobile u-responsive-hidden--show-mobile--inline"> Dev</span>
-              <span className="u-responsive-hidden--hide-mobile u-responsive-hidden--hide-mobile--inline"> Development</span>
-            </h1>
-            <p>I build startups and companies great <b>websites & apps</b>.</p>
-
-            <NavLink className="c-button" to="/about">
-              Learn more
-            </NavLink>
-
-          </div>
+          <NavLink className="c-button" to="/about">
+            Learn more
+          </NavLink>
 
         </div>
 
-        <Linebreak />
       </div>
 
-    )
-
-  }
-
+      <Linebreak />
+    </div>
+  )
 }
 
 export default Hero;
