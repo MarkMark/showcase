1e33e7bb1ae6a69c46ed520581b2e7203fb6f3ff
Mark <hisshash@gmail.com>
2019-01-18 17:55:41 -0500

    refactor (header): jsx

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,69 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
-import classNames from 'classnames'
 
 
-class Header extends Component {
+function Header() {
+  return (
+    <header className="c-header">
+      <div className="c-header__wrapper">
 
-  constructor(props) {
-    super(props);
+        <NavLink to="/" className="c-header__logo">
+          mshowcase.
+        </NavLink>
 
-    this.state = {
-      navActive  : false,
-      navAnimate : false
-    };
-  }
+        <nav className="c-header__nav">
+          <ul>
+            <li><NavLink to="/">home</NavLink></li>
+            <li><NavLink to="/about">about</NavLink></li>
+            <li><NavLink to="/showcase">showcase</NavLink></li>
+            <li><NavLink to="/contact">contact</NavLink></li>
+          </ul>
+        </nav>
 
-  // triggerNav () {
-  //   let { navActive, navAnimate } = this.state
-
-  //   if ( !navActive ) {
-  //     this.setState({ navActive: !navActive })
-
-  //     setTimeout(() => {
-  //       this.setState({ navAnimate: !navAnimate })
-  //     }, 60)
-
-  //   } else {
-  //     this.setState({ navAnimate: !navAnimate })
-
-  //     setTimeout(() => {
-  //       this.setState({ navActive: !navActive })
-  //     }, 60)
-  //   }
-  // }
-
-  render() {
-
-    let { navActive, navAnimate } = this.state
-
-    return (
-
-      <header className="c-header">
-        <div className="c-header__wrapper">
-
-          <NavLink to="/" className="c-header__logo">
-            mshowcase.
-          </NavLink>
-
-          {/* <i className="o-icon--menu c-header__icon" onClick={ () => this.triggerNav() }></i> */}
-
-          <nav className="c-header__nav">
-            <ul>
-              <li><NavLink to="/">home</NavLink></li>
-              <li><NavLink to="/about">about</NavLink></li>
-              <li><NavLink to="/showcase">showcase</NavLink></li>
-              <li><NavLink to="/contact">contact</NavLink></li>
-            </ul>
-          </nav>
-
-        </div>
-      </header>
-
-    )
-
-  }
-
+      </div>
+    </header>
+  )
 }
 
-export default Header;
+export default Header
