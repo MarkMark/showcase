e21ad7e8d8a3fd57a2b7d8cfdcf5fee614cfa522
Mark <hisshash@gmail.com>
2019-03-14 23:57:59 -0400

    Refactor (about): atomic

--- src/Scenes/Home/Homepage.js
+++ src/Scenes/Home/Homepage.js
@@ -6,28 +6,21 @@ import Linebreak from '../../Components/Linebreak/Linebreak'
 
 import { Thumbs } from '../../Services/Doc/Thumb'
 
-
 class Home extends Component {
-
   render() {
     return (
       <section>
-
         <Hero />
-
-        <Linebreak />
 
         <About />
 
-        <Linebreak />
-
-        <div className="o-wrapper">
+        {/* <div className="o-wrapper">
           <div className="m-grid">
             { Thumbs.map((object, i) => i < 4 &&
               <Thumb data={object} key={i} />
             )}
           </div>
-        </div>
+        </div> */}
       </section>
     )
   }
--- src/Scenes/Home/components/About.js
+++ src/Scenes/Home/components/About.js
@@ -1,25 +1,32 @@
 import React from 'react'
 
-function About() {
+export default function About() {
   return (
-    <section className="o-wrapper o-wrapper--small u-margin--bottom">
-      <div>
-        <p>Hey I’m Mark.</p>
-        <p>Developing websites & apps is my passion. I specialise in front-end development, focusing on all responsive viewport sizes while using the latest tools & frameworks to improve speed and quality of a project.</p>
-      </div>
+    <section className="o-wrapper u-padding-vertical-huge">
+      <div className="o-layout o-layout--centered">
+        <div className="o-col o-col--medium">
+          <div className="u-margin-bottom-large">
+            <p>Hey I’m Mark.</p>
+            <p>
+              Developing websites & apps is my passion. I specialise in
+              front-end development, focusing on all responsive viewport sizes
+              while using the latest tools & frameworks to improve speed and
+              quality of a project.
+            </p>
+          </div>
 
-      <div className="u-text-align--center">
-        <span className="c-pill">React</span>
-        <span className="c-pill">React Native</span>
-        <span className="c-pill">Angular</span>
-        <span className="c-pill">Vanilla JS</span>
-        <span className="c-pill">HTML</span>
-        <span className="c-pill">CSS</span>
-        <span className="c-pill">SCSS</span>
-        <span className="c-pill">LESS</span>
+          <div className="u-text-align--center">
+            <span className="c-pill">React</span>
+            <span className="c-pill">React Native</span>
+            <span className="c-pill">Angular</span>
+            <span className="c-pill">Vanilla JS</span>
+            <span className="c-pill">HTML</span>
+            <span className="c-pill">CSS</span>
+            <span className="c-pill">SCSS</span>
+            <span className="c-pill">LESS</span>
+          </div>
+        </div>
       </div>
     </section>
   )
 }
-
-export default About;
