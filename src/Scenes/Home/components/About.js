8544ebd5468549eb658e450bc910e91c97a3f0b0
Mark <hisshash@gmail.com>
2019-03-15 11:32:28 -0400

    Fix (about): background color

--- src/Scenes/Home/components/About.js
+++ src/Scenes/Home/components/About.js
@@ -14,23 +14,25 @@ export default function About() {
   ]
 
   return (
-    <section className="o-wrapper u-padding-vertical-huge s-skew">
-      <div className="o-layout o-layout--centered">
-        <div className="o-col o-col--medium">
-          <div className="u-margin-bottom-large">
-            <p>Hey I’m Mark.</p>
-            <p>
-              Developing websites & apps is my passion. I specialise in
-              front-end development, focusing on all responsive viewport sizes
-              while using the latest tools & frameworks to improve speed and
-              quality of a project.
-            </p>
-          </div>
+    <section className="s-skew">
+      <div className="o-wrapper u-padding-vertical-huge ">
+        <div className="o-layout o-layout--centered">
+          <div className="o-col o-col--medium">
+            <div className="u-margin-bottom-large">
+              <p>Hey I’m Mark.</p>
+              <p>
+                Developing websites & apps is my passion. I specialise in
+                front-end development, focusing on all responsive viewport sizes
+                while using the latest tools & frameworks to improve speed and
+                quality of a project.
+              </p>
+            </div>
 
-          <div className="u-text-align--center">
-            {EXPERTISE.map((skill, i) => {
-              return <Pill key={i} text={skill} />
-            })}
+            <div className="u-text-align--center">
+              {EXPERTISE.map((skill, i) => {
+                return <Pill key={i} text={skill} />
+              })}
+            </div>
           </div>
         </div>
       </div>
