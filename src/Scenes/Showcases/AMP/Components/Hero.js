026c4dfb307a9aeb10c35d18b350665006376912
Mark <hisshash@gmail.com>
2019-03-16 16:44:07 -0400

    Refactor (layout): amp

--- src/Scenes/Showcases/AMP/Components/Hero.js
+++ src/Scenes/Showcases/AMP/Components/Hero.js
@@ -1,46 +1,14 @@
-import React, { Component } from "react";
+import React from 'react'
 
-// import WEBPamp              from '../../../../img/showcase/amp/amp-feature.webp'
-import PNGamp from "../../../../img/showcase/amp/amp-feature.png";
-
-class Hero extends Component {
-  render() {
-    return (
-      <div className="c-hero c-hero--amp">
-        <div className="o-wrapper">
-          <h1>AMP shopping centres</h1>
-          <p>A retrospective showcase</p>
-
-          <div className="u-text-align--center u-margin-bottom-large">
-            <a
-              href="https://www.pacificfair.com.au/"
-              className="c-button c-button--white"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Pacific Fair
-            </a>
-
-            <a
-              href="https://www.macquariecentre.com.au"
-              className="c-button c-button--white"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              macquarie centre
-            </a>
-          </div>
-
-          <img
-            ref={img => (this.img = img)}
-            src={PNGamp}
-            onError={() => (this.img.src = PNGamp)}
-            alt="AMP"
-          />
+export default function Hero() {
+  return (
+    <section className="m-hero m-hero--medium">
+      <div className="m-hero__content">
+        <div>
+          <span>Web CMS</span>
+          <h1 className="u-weight--medium">AMP shopping centres</h1>
         </div>
       </div>
-    );
-  }
+    </section>
+  )
 }
-
-export default Hero;
