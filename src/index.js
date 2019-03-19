d4f7ca2266cca4743a648004bb4bc1015ac1a6db
Mark <hisshash@gmail.com>
2019-03-18 23:03:36 -0400

    Chore (v01): release

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react'
 // import ReactDOM from 'react-dom';
-import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "./scss/showcase.scss";
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import './scss/showcase.scss'
 
 render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
-registerServiceWorker();
+serviceWorker.unregister()
