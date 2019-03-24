170ec16e868a01d676b4f81f3e69e4a33e9117fd
Mark <hisshash@gmail.com>
2019-03-23 22:43:03 -0400

    Refactor (webp): thumbnails

--- src/Services/Doc/Thumb.js
+++ src/Services/Doc/Thumb.js
@@ -1,154 +1,152 @@
-// import ThumbWebpSanctuary from '../../img/thumbs/sanctuary.webp'
-import ThumbJpgSanctuary from '../../img/thumbs/sanctuary.jpg'
-// import ThumbWebpSanctuaryApp from '../../img/thumbs/sanctuary-app.webp'
-import ThumbJpgSanctuaryApp from '../../img/thumbs/sanctuary-app.jpg'
-// import ThumbWebpZensation from '../../img/thumbs/zensation.webp'
-import ThumbJpgZensation from '../../img/thumbs/zensation.jpg'
-// import ThumbWebpAmp from '../../img/thumbs/amp.webp'
-import ThumbJpgAmp from '../../img/thumbs/amp.jpg'
-// import ThumbWebpCaltex from '../../img/thumbs/caltex.webp'
-// import ThumbJpgCaltex from '../../img/thumbs/caltex.jpg'
-// import ThumbWebpBetfair from '../../img/thumbs/betfair.webp'
-import ThumbJpgBetfair from '../../img/thumbs/betfair.jpg'
-// import ThumbWebpCooltrax from '../../img/thumbs/cooltrax.webp'
-import ThumbJpgCooltrax from '../../img/thumbs/cooltrax.jpg'
-// import ThumbWebpCooltraxApp from '../../img/thumbs/cooltrax-app.webp'
-import ThumbJpgCooltraxApp from '../../img/thumbs/cooltrax-app.jpg'
-// import ThumbWebpEmpire from '../../img/thumbs/empire.webp'
-import ThumbJpgEmpire from '../../img/thumbs/empire.jpg'
-// import ThumbWebpEnracha from '../../img/thumbs/enracha.webp'
-import ThumbJpgEnracha from '../../img/thumbs/enracha.jpg'
-// import ThumbWebpFiction from '../../img/thumbs/fiction-candy.webp'
-import ThumbJpgFiction from '../../img/thumbs/fiction-candy.jpg'
-// import ThumbWebpFoxwoods from '../../img/thumbs/foxwoods.webp'
-import ThumbJpgFoxwoods from '../../img/thumbs/foxwoods.jpg'
-// import ThumbWebpJetstar from '../../img/thumbs/jetstar.webp'
-import ThumbJpgJetstar from '../../img/thumbs/jetstar.jpg'
-// import ThumbWebpKearAndKu from '../../img/thumbs/kear-and-ku.webp'
-import ThumbJpgKearAndKu from '../../img/thumbs/kear-and-ku.jpg'
-// import ThumbWebpMendr from '../../img/thumbs/mendr.webp'
-import ThumbJpgMendr from '../../img/thumbs/mendr.jpg'
-// import ThumbWebpMoneyGaming from '../../img/thumbs/money-gaming.webp'
-import ThumbJpgMoneyGaming from '../../img/thumbs/money-gaming.jpg'
-// import ThumbWebpRunaway from '../../img/thumbs/runaway-pets.webp'
-import ThumbJpgRunaway from '../../img/thumbs/runaway-pets.jpg'
-import ThumbJpgTchoi from '../../img/thumbs/tchoidesign.jpg'
-// import ThumbWebpTchoi from '../../img/thumbs/tchoidesign.webp'
+import AMP from '../../img/thumbs/amp.webp'
+import AMPFallback from '../../img/thumbs/amp.jpg'
+import Betfair from '../../img/thumbs/betfair.webp'
+import BetfairFallback from '../../img/thumbs/betfair.jpg'
+import Cooltrax from '../../img/thumbs/cooltrax.webp'
+import CooltraxFallback from '../../img/thumbs/cooltrax.jpg'
+import CooltraxApp from '../../img/thumbs/cooltrax-app.webp'
+import CooltraxAppFallback from '../../img/thumbs/cooltrax-app.jpg'
+import Empire from '../../img/thumbs/empire.webp'
+import EmpireFallback from '../../img/thumbs/empire.jpg'
+import Enracha from '../../img/thumbs/enracha.webp'
+import EnrachaFallback from '../../img/thumbs/enracha.jpg'
+import Fiction from '../../img/thumbs/fiction-candy.webp'
+import FictionFallback from '../../img/thumbs/fiction-candy.jpg'
+import Foxwoods from '../../img/thumbs/foxwoods.webp'
+import FoxwoodsFallback from '../../img/thumbs/foxwoods.jpg'
+import Jetstar from '../../img/thumbs/jetstar.webp'
+import JetstarFallback from '../../img/thumbs/jetstar.jpg'
+import KearAndKu from '../../img/thumbs/kear-and-ku.webp'
+import KearAndKuFallback from '../../img/thumbs/kear-and-ku.jpg'
+import Mendr from '../../img/thumbs/mendr.webp'
+import MendrFallback from '../../img/thumbs/mendr.jpg'
+import MoneyGaming from '../../img/thumbs/money-gaming.webp'
+import MoneyGamingFallback from '../../img/thumbs/money-gaming.jpg'
+import Runaway from '../../img/thumbs/runaway-pets.webp'
+import RunawayFallback from '../../img/thumbs/runaway-pets.jpg'
+import Sanctuary from '../../img/thumbs/sanctuary.webp'
+import SanctuaryFallback from '../../img/thumbs/sanctuary.jpg'
+import SanctuaryApp from '../../img/thumbs/sanctuary-app.webp'
+import SanctuaryAppFallback from '../../img/thumbs/sanctuary-app.jpg'
+import Tchoi from '../../img/thumbs/tchoidesign.webp'
+import TchoiFallback from '../../img/thumbs/tchoidesign.jpg'
+import Zensation from '../../img/thumbs/zensation.webp'
+import ZensationFallback from '../../img/thumbs/zensation.jpg'
 
 export const Thumbs = [
   {
     name: 'Sanctuary app',
     desc:
       'Sanctuary is a new way of making sure all your employees are safe by sending push notifications to your employees and getting a real-time response back.',
-    // src: ThumbWebpSanctuaryApp,
-    src: ThumbJpgSanctuaryApp
+    src: SanctuaryApp,
+    srcFallback: SanctuaryAppFallback
   },
   {
     name: 'Sanctuary',
     desc:
       'Sanctuary is a new way of making sure all your employees are safe by sending push notifications to your employees and getting a real-time response back.',
-    // src: ThumbWebpSanctuary,
-    src: ThumbJpgSanctuary
+    src: Sanctuary,
+    srcFallback: SanctuaryFallback
   },
   {
     name: 'AMP',
     desc: `AMP Capital is Australia's largest retail and corporate pension providers & own around 30 shopping malls across Australia & New Zealand.`,
-    // src: ThumbWebpAmp,
-    src: ThumbJpgAmp
+    src: AMP,
+    srcFallback: AMPFallback
   },
   {
     name: 'Zensation',
     desc:
       "Zensation is a popular Tea House in Redfern, Sydney. Echoing the stylings of a 1920s Shanghai Tea House. It's a truly unique experience to sit down relax and, try the best tea in Australia while eating some delicious Yum Cha!",
-    // src: ThumbWebpZensation,
-    src: ThumbJpgZensation
+    src: Zensation,
+    srcFallback: ZensationFallback
   },
   {
     name: 'Runaway pets',
     desc:
       "Over 10 million pets run away every year & only 1 in 10 are ever reunited with their owners. So what do you do when you lose a pet? There’s not really a ,manual written for something like this. A quick google search will tell you to contact shelters, vets, etc. Then start posting on social media and hope it catches someone's attention. You may even post flyers around your block.",
-    // src: ThumbWebpRunaway,
-    src: ThumbJpgRunaway
+    src: Runaway,
+    srcFallback: RunawayFallback
   },
   {
     name: 'mendr',
     desc:
       'mendr was formerly trading under the name of BevWizz, where for years they were fixing the big restaurant groups and franchised hospitality business ,equipment. The mission of its CEO, Justin Morris, was that they wanted to expand this service to cater for everyone, from your huge fast food chain to your local chippy.',
-    // src: ThumbWebpMendr,
-    src: ThumbJpgMendr
+    src: Mendr,
+    srcFallback: MendrFallback
   },
   {
     name: 'cooltrax app',
     desc:
       'Cooltrax the app was an iOS & Android app built for the technicians working with the company BevWizz. BevWizz needed a way to know what job a technician was ,currently doing and or going to do in that day. They also needed to know how long the job would take, what was the problem, how much would/ did it cost to complete the work, where they currently was, etc.',
-    // src: ThumbWebpCooltraxApp,
-    src: ThumbJpgCooltraxApp
+    src: CooltraxApp,
+    srcFallback: CooltraxAppFallback
   },
   {
     name: 'cooltrax',
     desc: '',
-    // src: ThumbWebpCooltrax,
-    src: ThumbJpgCooltrax
+    src: Cooltrax,
+    srcFallback: CooltraxFallback
   },
   {
     name: 'Jetstar',
     desc:
       'Jetstar Airways Pty Ltd, trading as Jetstar, is an Australian low-cost airline headquartered in Melbourne. It is a wholly owned subsidiary of Qantas, created ,in response to the threat posed by low-cost airline Virgin Australia.',
-    // src: ThumbWebpJetstar,
-    src: ThumbJpgJetstar
+    src: Jetstar,
+    srcFallback: JetstarFallback
   },
   {
     name: 'Tchoi design',
     desc:
       'Tchoi design is my ex colleague and good friend, Jeremie Erimitchoi’s, web design portfolio. Myself and Jeremie worked together at GameAccount Network in ,London, where Jeremie was my front-end teams web designer. Jeremie approached me in 2015 to build him a simple and clean website where he could showcase all his web and logo design work.',
-    // src: ThumbWebpTchoi ,
-    src: ThumbJpgTchoi
+    src: Tchoi,
+    srcFallback: TchoiFallback
   },
   {
     name: 'Betfair',
     desc:
       'Betfair Casino New Jersey offers a variety of fun NJ online casino gambling games from Blackjack tables, to slots and Roulette. Join today for your chance to, win big.',
-    // src: ThumbWebpBetfair,
-    src: ThumbJpgBetfair
+    src: Betfair,
+    srcFallback: BetfairFallback
   },
   {
     name: 'Empire',
     desc:
       'Empire City Online Casino is free to play and built for entertainment, so you can experience real casino games for free, in the comfort of your own home.',
-    // src: ThumbWebpEmpire,
-    src: ThumbJpgEmpire
+    src: Empire,
+    srcFallback: EmpireFallback
   },
   {
     name: 'Foxwoods',
     desc:
       'Since 1992, Foxwoods Resort Casino has offered guests The Wonder Of It All as the premier resort destination in the Northeast. As the largest resort casino in, North America, Foxwoods offers a vast array of gaming in seven casinos; AAA Four-Diamond hotels, restaurants from gourmet to quick service, world-renowned spas, award-winning golf, state-of-the-art theaters, exclusive retailers and free-to-play online casino games at FoxwoodsONLINE.com.',
-    // src: ThumbWebpFoxwoods,
-    src: ThumbJpgFoxwoods
+    src: Foxwoods,
+    srcFallback: FoxwoodsFallback
   },
   {
     name: 'Enracha',
     desc:
       "Enracha. Rank's community-based gaming business for the Spanish market. The brand offers a range of popular community games like bingo as well as electronic, roulette and slot machine games, sports betting and food, drink and live entertainment.",
-    // src: ThumbWebpEnracha,
-    src: ThumbJpgEnracha
+    src: Enracha,
+    srcFallback: EnrachaFallback
   },
   {
     name: 'MoneyGaming',
     desc:
       'MoneyGaming.com is one of the UK’s most popular casino gaming websites, online since 2005 and part of the GameAccount Network group of companies since 2010. Established in 2002, GameAccount Network is a UK company headquartered in London, United Kingdom and ranked 29th fastest-growing UK technology company in 2011 by the Sunday Times newspaper.',
-    // src: ThumbWebpMoneyGaming,
-    src: ThumbJpgMoneyGaming
+    src: MoneyGaming,
+    srcFallback: MoneyGamingFallback
   },
   {
     name: 'Fiction candy jewellery',
     desc: '',
-    // src: ThumbWebpFiction,
-    src: ThumbJpgFiction
+    src: Fiction,
+    srcFallback: FictionFallback
   },
   {
     name: 'Kear and ku',
     desc: '',
-    // src: ThumbWebpKearAndKu,
-    src: ThumbJpgKearAndKu
+    src: KearAndKu,
+    srcFallback: KearAndKuFallback
   }
 ]
